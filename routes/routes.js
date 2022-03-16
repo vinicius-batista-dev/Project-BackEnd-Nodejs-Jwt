0d40bf3719fa1c4c8a25672e6534ff91f5c814a1
vinicius-batista-dev <20201012000256@pucgo.edu.br>
2022-03-16 14:43:28 -0300

    Adicionando o findUser

--- routes/routes.js
+++ routes/routes.js
@@ -7,6 +7,6 @@ var UserController = require('../controllers/UserController');
 router.get('/', HomeController.index);
 router.post('/user', UserController.create);
 router.get('/user', UserController.index);  
-router.get('/users/:id', UserController.findUser);
+router.get('/user/:id', UserController.findUser);
 
 module.exports = router
