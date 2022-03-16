7a071698bb670ba1ed7d96dd75ba29b52b18e46c
vinicius-batista-dev <20201012000256@pucgo.edu.br>
2022-03-16 09:36:39 -0300

    retornando o status quando tiver o email vazio

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,8 +2,15 @@ class UserController{
     async index(req, res){}
 
     async create(req, res){
-        console.log(req.body);
-        res.send("Pegando o corpo da requisicao")
+        var {email, name, password} = req.body;
+
+        //Validando o Email
+
+        if(email == undefined){
+            res.status(403);
+            res.json({err: "O email é invalido."})
+        }
+        res.send("Pegando o corpo da requisicao.")
     }
 }
 
